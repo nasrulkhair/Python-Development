b6ca4233e9315e5a4bc5a0d6bb8f4f4eaaca956d
Nasrul Khair <nasrulkhair@yahoo.com>
2024-08-23 10:08:15 +0800

    modified passWordManager.py and relocate file

---
+++ Project_Python/gameProject/passworManager.py
@@ -0,0 +1,31 @@
+master_pwd = input("What is the master password? ")
+
+#defining function
+def view():
+    #open the file and select the mode (a/r/w)
+    with open("password.txt","r") as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print(f"User:{user}| Password:{passw}")
+
+def add():
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+    
+    #open the file and select the mode (a/r/w)
+    with open("password.txt","a") as f:
+        f.write(f"{name} | {pwd}\n")
+
+while True:
+    mode = input("Would you like to add a new password or view existing ones (view,add), press q to quit? ").lower()
+    if mode == "q":
+        break
+    
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode")
+        continue
--- Project_Python/gameProject/tempCodeRunnerFile.py
+++ Project_Python/gameProject/tempCodeRunnerFile.py
@@ -1,44 +1,31 @@
-import random
+pwd = input("What is the master password? ")
 
-user_score = 0
-computer_score = 0
+#defining function
+def view():
+    #open the file and select the mode (a/r/w)
+    with open("password.txt","r") as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.strip("|")
+            print(f"User: {user}, Password: {passw}")
 
-options = ["rock", "paper", "scissors"]
-#options = options[0]
-#print(options)
+def add():
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+    
+    #open the file and select the mode (a/r/w)
+    with open("password.txt","a") as f:
+        f.write(f"{name} | {pwd}\n")
 
 while True:
-    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
-    if user_input == "q":
+    mode = input("Would you like to add a new password or view existing ones (view,add), press q to quit? ").lower()
+    if mode == "q":
         break
     
-    if user_input not in options:
-        print("Please enter a valid option :)")
-        continue
-    
-    random_number = random.randint(0,2)
-    # rock - 0, paper - 1, scissors - 2
-    computer_pick = options[random_number]
-    print(f"Computer pick, {computer_pick}." )
-    
-    if user_input == "rock" and computer_pick == "scissors":
-        print("You won!")
-        user_score += 1
-    elif user_input == "paper" and computer_pick == "rock":
-        print("You won!")
-        user_score += 1
-    elif user_input == "scissors" and computer_pick == "paper":
-        print("You won!")
-        user_score += 1
-    elif user_input == computer_pick:
-        print("Its a draw!")
-        user_score += 1
-        computer_score += 1
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
     else:
-        print("You lost :(")
-        computer_score += 1
-
-print(f"You won, {user_score} times.")
-print(f"Computer won, {computer_score} times.")
-print("Goodbye")
-    
+        print("Invalid mode")
+        continue
