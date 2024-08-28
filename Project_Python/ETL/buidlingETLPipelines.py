e97f8c04b2458d1cab48395d8299351a1a16354a
Nasrul Khair <nasrulkhair@yahoo.com>
2024-08-28 12:31:44 +0800

    commenting all

--- Project_Python/ETL/buidlingETLPipelines.py
+++ Project_Python/ETL/buidlingETLPipelines.py
@@ -1,3 +1,4 @@
+"""""
 #PERSISTING DATA TO FILES
 
 # Import the os library
@@ -79,3 +80,4 @@ except KeyError as ke:
 	# Create the "Total Price" column, transform the updated DataFrame
 	raw_sales_data["Total Price"] = raw_sales_data["Price Each"] * raw_sales_data["Quantity Ordered"]
 	clean_sales_data = transform(raw_sales_data)
+"""
