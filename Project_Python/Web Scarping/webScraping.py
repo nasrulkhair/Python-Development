2000c31530df536091a9be715bde02ca78a22ed0
Nasrul Khair <nasrulkhair@yahoo.com>
2024-09-02 16:43:50 +0800

    features: webScraping.py

---
+++ Project_Python/Web Scarping/webScraping.py
@@ -0,0 +1,27 @@
+
+'''
+xpath = '/html/body/div[2]'  --> #look for specific generations
+xpath = '//table'   --> #look forward allfuture generations
+xpath = '/html/body/div[2]//table' --> #direct to all table elements which are descendants of the 2nd div child of the body
+'''
+
+from scrapy import Selector
+
+html = '''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DataCamp Example</title>
+</head>
+<body>
+    <div class="hello datacamp">
+        <p>Hello World!</p>
+    </div>
+    <p>Enjoy DataCamp!</p>
+</body>
+</html>
+'''
+
+sel = Selector( text = html )
+paragraphs = sel.xpath("//p/text()").extract()
+print(paragraphs)
