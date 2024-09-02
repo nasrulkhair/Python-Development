65858a2b431aded3fc3142b21228bece1149d5fe
Nasrul Khair <nasrulkhair@yahoo.com>
2024-09-02 16:56:09 +0800

    features webScraping.py

---
+++ Project_Python/webScraping/webScraping.py
@@ -0,0 +1,20 @@
+from scrapy import Selector
+
+html = '''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DataCamp Example</title>
+</head>
+<body>
+    <div class="hello datacamp">
+        <p>Hello World!</p>
+    </div>
+    <p>Enjoy DataCamp!</p>
+</body>
+</html>
+'''
+
+sel = Selector(text=html)
+paragraphs = sel.xpath("//p/text()").getall()  # Recommended method
+print(paragraphs)
