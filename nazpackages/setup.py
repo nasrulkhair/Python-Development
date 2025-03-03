173afc09fae4ff7f446c077e92bd7ee9f7c0e0f0
Nasrul Khair <nasrulkhair@yahoo.com>
2025-03-03 13:22:03 +0800

    Relocate packages into nazpacakges dir

---
+++ nazpackages/setup.py
@@ -0,0 +1,9 @@
+# Import required functions
+from setuptools import setup
+
+# Call setup function
+setup(
+    author="Nasrul Khair",
+    description="A bundles of packages im saving for to ease my daily tasks",
+    name="nazpackages"
+)
