064c749a73d96559d472a7d38b64593bb83da9d9
Nasrul Khair <nasrulkhair@yahoo.com>
2025-03-06 19:58:18 +0800

    mv to diff repo

--- PIPELINE Project/create_user.sql
+++
@@ -1,8 +0,0 @@
--- Create the user
-CREATE USER etl WITH PASSWORD '1234';
-
---Grant database access
-GRANT CONNECT ON DATABASE dvdrental TO etl;
-
---Grant table permissions
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO etl;
